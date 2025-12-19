0d5e03c87a63f6443aaa60b9491e99575f5df0bf
McNast13 <33765470+McNast13@users.noreply.github.com>
2025-12-19 21:34:08 +0000

    Initial Commit

---
+++ CityLiveRadio/CityLiveRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityLiveRadioApp.swift
+//  CityLiveRadio
+//
+//  Created by paul mackay on 19/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CityLiveRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityLiveRadioTests/CityLiveRadioTests.swift
@@ -0,0 +1,17 @@
+//
+//  CityLiveRadioTests.swift
+//  CityLiveRadioTests
+//
+//  Created by paul mackay on 19/12/2025.
+//
+
+import Testing
+@testable import CityLiveRadio
+
+struct CityLiveRadioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
